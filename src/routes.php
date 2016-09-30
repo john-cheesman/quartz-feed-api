fa25bd4719d812a17f116c6c32ad7f7839bb9750
John Cheesman <me@johncheesman.org.uk>
2016-10-01 00:55:34 +0100

    Add PicoFeed and setup getFeed route

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,33 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+use PicoFeed\Reader\Reader;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->get('/getFeed', function (Request $request, Response $response) {
+    try {
+        $url = $request->getQueryParam('url');
+        $reader = new Reader;
+        $resource = $reader->download($url);
+
+        if ($resource) {
+            $parser = $reader->getParser(
+                $resource->getUrl(),
+                $resource->getContent(),
+                $resource->getEncoding()
+            );
+
+            $feed = $parser->execute();
+            $response->withStatus(200);
+            $response->withHeader('Content-Type', 'application/json');
+            echo json_encode($feed);
+        } else {
+            throw new PDOException('No feed data', 101);
+        }
+    } catch (Exception $e) {
+        $response->withStatus(404);
+        $this->logger->info('Error: ' . $e->getMessage() . ' Code: ' . $e->getCode());
+        echo '{"error":{"text":' . $e->getMessage() . ', "code":' . $e->getCode() . '}}';
+    }
 });
