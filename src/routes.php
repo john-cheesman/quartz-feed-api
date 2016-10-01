62d3314c4e7bbade4d381320d4a431c9f4d48fac
John Cheesman <me@johncheesman.org.uk>
2016-10-01 21:34:42 +0100

    Add getAvailableFeeds method

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,7 @@
 // Routes
 
 use PicoFeed\Reader\Reader;
+use PicoFeed\PicoFeedException;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -25,9 +26,33 @@ $app->get('/getFeed', function (Request $request, Response $response) {
         } else {
             throw new PDOException('No feed data', 101);
         }
-    } catch (Exception $e) {
+    } catch (PicoFeedException $e) {
         $response->withStatus(404);
         $this->logger->info('Error: ' . $e->getMessage() . ' Code: ' . $e->getCode());
         echo '{"error":{"text":' . $e->getMessage() . ', "code":' . $e->getCode() . '}}';
     }
 });
+
+$app->get('/getAvailableFeeds', function (Request $request, Response $response) {
+    try {
+        $url = $request->getQueryParam('url');
+        $reader = new Reader;
+        $resource = $reader->download($url);
+
+        if ($resource) {
+            $feeds = $reader->find(
+                $resource->getUrl(),
+                $resource->getContent()
+            );
+
+            echo json_encode($feeds);
+        } else {
+            throw new PDOException('No available feeds', 102);
+        }
+    } catch (PicoFeedException $e) {
+        $response->withStatus(404);
+        $this->logger->info('Error: ' . $e->getMessage() . ' Code: ' . $e->getCode());
+        echo '{"error":{"text":' . $e->getMessage() . ', "code":' . $e->getCode() . '}}';
+    }
+});
+
